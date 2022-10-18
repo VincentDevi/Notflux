a909e1dfabbd335d643cc92fb95a01eb2f71338d
Devilers Vincent <vincent.devilers@gmail.com>
2022-10-18 16:43:57 +0200

    added values in tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+      fontFamily:{
+        'halloween' : ['Creepster', 'cursive'],
+        'netflix' : ['Bebas Neue', 'cursive'],
+      },
+
+      colors:{
+        'red' : "#db0000",
+        'black' : "#000000",
+        'grey' : "#564d4d",
+        'bordeaux' : "#831010",
+      },
+      fontSize:{
+        'enormous' : '3rem',
+        'xtra' : "2rem",
+        'large' : "1.5rem",
+        'medium' : "1rem",
+        'small' : "0.75rem",
+      },
     extend: {},
   },
   plugins: [],
